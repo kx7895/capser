fd578e8dfa72dccdbf1ed28569f58fb504a8ee3c
kx7895 <51049653+kx7895@users.noreply.github.com>
2024-05-04 15:28:23 +0200

    Testweise Aktivierung "persist sidebar toggle between refreshes"

--- assets/libs/sbadmin/js/scripts.js
+++ assets/libs/sbadmin/js/scripts.js
@@ -7,10 +7,9 @@
     // Toggle the side navigation
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
     if (sidebarToggle) {
-        // Uncomment Below to persist sidebar toggle between refreshes
-        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        //     document.body.classList.toggle('sidenav-toggled');
-        // }
+        if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+            document.body.classList.toggle('sidenav-toggled');
+        }
         sidebarToggle.addEventListener('click', event => {
             event.preventDefault();
             document.body.classList.toggle('sidenav-toggled');
