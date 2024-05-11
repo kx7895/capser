e5d0aea6e691cd2b5a4daa336cea2a0723eaffd6
kx7895 <51049653+kx7895@users.noreply.github.com>
2024-05-11 16:53:21 +0200

    Updated CSS styles, layout and form changes
    Made several modifications to styling and layout in multiple files. Made changes to the user interface for guest and customer pages, and improved mobile responsiveness.

--- assets/guest.js
+++ assets/guest.js
@@ -1,7 +1,6 @@
 import './bootstrap.js';
 
-import './libs/sbadmin/css/styles.css'; // bundle from SB Admin includes regular Bootstrap CSS file
-import './libs/sbadmin/js/scripts.js';
+import './libs/sbadmin/css/styles.css'; // bundle SB Admin Pro v2.0.5 + Bootstrap  v5.2.3
 
 import './styles/capser.css';
 import './styles/capser-guest.css';
